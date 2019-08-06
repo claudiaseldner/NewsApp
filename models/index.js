da2fd5a0570f339b0bc9ae4b9569a14e8b3fe87e
Claudia Seldner <claudiaseldner@Claudias-MacBook-Pro-2.local>
2019-08-05 21:47:57 -0500

    adding route

--- models/index.js
+++ models/index.js
@@ -1,7 +1,7 @@
 // Exporting an object containing all of our models
 
 module.exports = {
-    Article: require("./Article.js"),
-    Note: require("./Note.js")
+    Article: require("../models/Article"),
+    Note: require("../models/Note")
   };
   
